0bf5658f13cb63433268646b9b631da0481d8f1b
Lsig <logis21@ru.is>
2023-01-24 15:40:18 +0000

    Ditched file explorer and migrated to lazy instead of packer

---
+++ lua/lsig/lazy.lua
@@ -0,0 +1,67 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+return require("lazy").setup({
+	-- Packer can manage itself
+	"folke/tokyonight.nvim",
+	-- lsp config
+	{
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	"onsails/lspkind.nvim",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	},
+	{
+		"glepnir/lspsaga.nvim",
+		event = "BufRead",
+		config = function()
+			require("lspsaga").setup({})
+		end,
+	},
+	-- Telescope
+	{
+		"nvim-telescope/telescope.nvim",
+		version = "0.1.0",
+		dependencies = { { "nvim-lua/plenary.nvim" } },
+	},
+	-- linter
+	"mfussenegger/nvim-lint",
+	-- Autocomplete
+	{ "L3MON4D3/LuaSnip", version = "v<CurrentMajor>.*" },
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"saadparwaiz1/cmp_luasnip",
+		},
+	},
+	{
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	},
+	-- Tpope and more
+	{ "tpope/vim-surround", event = "VeryLazy" },
+	{ "tpope/vim-commentary", event = "VeryLazy" },
+})
